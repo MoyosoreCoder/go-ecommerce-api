d8c4420142db90c127ade282aa9d1a21b7b5e494
MoyosoreCoder <kelanimaryam1234@gmail.com>
2026-02-21 15:30:00 +0100

    internal database connection set up properly

--- internal/database/db.go
+++ internal/database/db.go
@@ -1,12 +1,32 @@
+// connecting database import package database first
 package database
+
 import (
 	"database/sql"
 	"log"
+	"os"
+
 	_ "github.com/lib/pq"
 )
 
-var DB */sql/DB
+var DB *sql.DB
 
-func connect(){
-	
+func Connect(){
+	//get conncetion string from environment variables
+	connStr := os.Getenv("DB_URL")
+	if connStr == "" {
+		log.Fatal("DB_URL environment variable not set")
+	}
+	var err error
+	DB, err = sql.Open("postgres", connStr)
+	if err != nil{
+		log.Fatal("Error opening database:", err)
+
+	}
+	// verify error
+	err = DB.Ping()
+	if err != nil {
+		log.Fatal("Error connecting to database", err)
+	}
+	log.Println("Database connected successfully")
 }
