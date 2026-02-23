03adf436d906ac25e1b9757d5a43a803e27cb9cd
MoyosoreCoder <kelanimaryam1234@gmail.com>
2026-02-23 11:14:32 +0100

    environment variable using the connection string from .env

--- internal/database/db.go
+++ internal/database/db.go
@@ -5,20 +5,37 @@ import (
 	"database/sql"
 	"log"
 	"os"
-
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
+	"fmt"
 )
 
 var DB *sql.DB
 
 func Connect(){
-	//get conncetion string from environment variables
-	connStr := os.Getenv("DB_URL")
-	if connStr == "" {
-		log.Fatal("DB_URL environment variable not set")
+	//Load env variables
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Println("No .env file found, using system env variables")
 	}
-	// now open the database
-	var err error
+
+	// Read environment variables
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+
+	// Check that all variables exist
+	if host == "" || port == "" || user == "" || password == "" || dbname == "" {
+		log.Fatal("One or more DB environment variables not set")
+	}
+	
+	// Build connection string
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname,)
+	
+	// Open connection String
 	DB, err = sql.Open("postgres", connStr)
 	if err != nil{
 		log.Fatal("Error opening database:", err)
