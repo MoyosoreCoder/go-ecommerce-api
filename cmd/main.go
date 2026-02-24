d0e7d35be39d8611b7f020d8023d588237ab3e42
MoyosoreCoder <kelanimaryam1234@gmail.com>
2026-02-24 18:35:08 +0100

    main server set up

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,19 @@
-http.HandleFunc("/register", handler.RegisterUserHandler)
+//package main
+package main
+import (
+	"net/http"
+	"log"
+	"github.com/MoyosoreCoder/go-ecommerce-api/internal/handler"
+)
+func main(){
+	// Register route
+	http.HandleFunc("/regiter", handler.RegisterUserHandler)
+
+	// Start server
+	log.Println("Server running on port 8080...")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
