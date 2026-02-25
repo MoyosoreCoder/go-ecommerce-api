57587c87f0035db7f46696f3d7269c555068dd5b
MoyosoreCoder <kelanimaryam1234@gmail.com>
2026-02-25 10:07:18 +0100

    the main server set up correctly

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,14 @@
 package main
 import (
 	"net/http"
-	"log"
+	"log" 
 	"github.com/MoyosoreCoder/go-ecommerce-api/internal/handler"
 )
 func main(){
 	// Register route
-	http.HandleFunc("/regiter", handler.RegisterUserHandler)
+	http.HandleFunc("/register", handler.RegisterUserHandler)
+	//login route
+	http.HandleFunc("/login", handler.LoginUserHandler)
 
 	// Start server
 	log.Println("Server running on port 8080...")
