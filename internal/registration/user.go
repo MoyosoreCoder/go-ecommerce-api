5c0542bf90c40e43829d2a42e37372518400e2fd
MoyosoreCoder <kelanimaryam1234@gmail.com>
2026-02-25 10:05:10 +0100

    logic for the registration and login user completed

--- internal/registration/user.go
+++ internal/registration/user.go
@@ -5,6 +5,7 @@ import (
 	"github.com/MoyosoreCoder/go-ecommerce-api/internal/database"
 	"golang.org/x/crypto/bcrypt"
 )
+// Register user function
 func RegisterUser(username, email, password string) error {
 	if username == "" || email == "" || password == "" {
 		return errors.New("all fields are required")
@@ -30,4 +31,35 @@ func RegisterUser(username, email, password string) error {
 	}
 	return  nil
 
+}
+// Login user function
+func LoginUser(email, password string) error {
+
+	if email == "" || password == "" {
+		return errors.New("email and password are required")
+	}
+
+	var storedHashedPassword string
+
+	// Get hashed password from DB
+	err := database.DB.QueryRow(
+		"SELECT password FROM users WHERE email = $1",
+		email,
+	).Scan(&storedHashedPassword)
+
+	if err != nil {
+		return errors.New("invalid email or password")
+	}
+
+	// Compare passwords
+	err = bcrypt.CompareHashAndPassword(
+		[]byte(storedHashedPassword),
+		[]byte(password),
+	)
+
+	if err != nil {
+		return errors.New("invalid email or password")
+	}
+
+	return nil
 }
