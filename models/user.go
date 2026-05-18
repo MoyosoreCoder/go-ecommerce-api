0cf6a23faf01f8d261a2d41ef4a6d494850205ba
MoyosoreCoder <kelanimaryam1234@gmail.com>
2026-05-18 11:43:07 +0100

    models folder for user

---
+++ models/user.go
@@ -0,0 +1,11 @@
+package models
+
+type RegisterUser struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+ type LoginUser struct {
+	Email string `json:"email"`
+	Password string `json:"password"`
+ }
