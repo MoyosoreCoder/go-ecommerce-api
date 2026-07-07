637a911a675dab18106bdfa115c92c817a9d305e
MoyosoreCoder <kelanimaryam1234@gmail.com>
2026-07-07 12:07:58 +0100

    working files deleted neatly

--- migrations/20260517060106_create_users.up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
-    username TEXT NOT NULL,
-    email TEXT UNIQUE NOT NULL,
-    password TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
