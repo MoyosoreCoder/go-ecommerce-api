77e376a4a151b6b69f166cec148671f72a051a4b
MoyosoreCoder <kelanimaryam1234@gmail.com>
2026-09-17 13:57:26 +0100

    main.go file added successfully

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"log"
 	"github.com/MoyosoreCoder/go-ecommerce-api/internal/database"
+	"github.com/MoyosoreCoder/go-ecommerce-api/internal/handlers"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -12,13 +13,17 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/auth/google", handlers.GoogleLoginHandler)
+	http.HandleFunc("/auth/google/callback", handlers.GoogleCallbackHandler)
+
 	pool, err := database.ConnectDB()
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer pool.Close()
 	
-	http.HandleFunc("/", homeHandler)
+	
 	fmt.Println("Server is running at http://localhost:8080")
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
